5bba7cc5ba1e989ebf83d6bba11db4d1bf320b8d
= <f007h30@dartmouth.edu>
2025-02-18 16:07:38 -0500

    Add part 2

--- Project_5.jl
+++ Project_5.jl
@@ -21,4 +21,26 @@ display(time_domain)
 freq_domain = plot(freqs, abs.(F), title="Spectrum", yscale=:log10, framestyle=:box)
 display(freq_domain)
 
-
+# --- 2
+using FFTW, Plots, StatGeochem
+data = importdataset("weekly_in_situ_co2_mlo.csv", ',', importas=:Tuple)
+# Signal: water Level
+wl = data.Water_Level
+# Number of sample points
+N = length(data.Hour_Count)
+# Sample period
+Ts = 24/N
+# Start and end time
+t0 = 0
+tmax = t0 + N * Ts
+# Time coordinate
+t = t0:Ts:tmax
+# Fast Fourier Transform
+ft = fft(wl)
+F = fftshift(ft)
+freqs = fftshift(fftfreq(N, 1.0/Ts))
+# Plot results
+time_domain = plot(t[1:end-1], wl, title="Signal", framestyle=:box)
+display(time_domain)
+freq_domain = plot(freqs, abs.(F), title="Spectrum", yscale=:log10, framestyle=:box)
+display(freq_domain)
